57cf749283c49d270ee1e2453738d044d582032a
blizer <phivip123123@gmail.com>
2019-12-27 18:42:48 +0700

    Update paddle.py

--- paddle.py
+++ paddle.py
@@ -1,6 +1,6 @@
 import pygame
 BLACK = (0,0,0)
-
+ 
 class Paddle(pygame.sprite.Sprite):
     #This class represents a car. It derives from the "Sprite" class in Pygame.
     
@@ -19,3 +19,15 @@ class Paddle(pygame.sprite.Sprite):
         
         # Fetch the rectangle object that has the dimensions of the image.
         self.rect = self.image.get_rect()
+        
+    def moveUp(self, pixels):
+        self.rect.y -= pixels
+		#Check that you are not going too far (off the screen)
+        if self.rect.y < 0:
+          self.rect.y = 0
+          
+    def moveDown(self, pixels):
+        self.rect.y += pixels
+	#Check that you are not going too far (off the screen)
+        if self.rect.y > 400:
+          self.rect.y = 400
